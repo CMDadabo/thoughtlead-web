ea104d3b4d9eae8971279c5bb9e831fa06053687
Vokal <vokal@Allens-MacBook-Pro.local>
2015-09-25 10:47:03 -0500

    Adding filtering from main navigation.

--- source/modules/mocks/mock-data.js
+++ source/modules/mocks/mock-data.js
@@ -17,15 +17,27 @@ angular.module( "e2e-mocks" )
         {
             "id": 1,
             "link": "http://techcrunch.com/2015/09/24/early-bird-tickets-to-disrupt-london-are-now-available/",
-            "teams": [ 1, 5 ],
+            "teams": 1,
             "thoughtleader": 43,
             "description": "Disrupt London",
-            "creation_date": "2015-09-24"
+            "creation_date": "2013-09-24"
+        },
+        {
+            "id": 3,
+            "link": "http://davidwalsh.name/impostor-syndrome",
+            "teams": [ 1, 2, 3, 4, 5 ],
+            "thoughtleader": 43,
+            "description": "I'm an Impostor",
+            "creation_date": "2015-05-06"
         }
 
     ],
 
     getTeams : [
+        {
+            "id": "",
+            "name": "All"
+        },
         {
             "id": 1,
             "name": "developers"
